a591fa1e38c0f756560addf7c8edc602efbe3ec3
Glarff <michael.ross444@gmail.com>
2023-08-12 19:44:20 +0000

    added missing files

---
+++ app/demo/page.tsx
@@ -0,0 +1,8 @@
+// `app/demo/page.tsx` is the UI for the `/demo` URL
+export default function Page() {
+    return (
+        <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+            Hello, Demo Page!
+        </div>
+    )
+  }
