a5e5c2aba227c3765cbafe00aa294c3ef040f468
Glarff <michael.ross444@gmail.com>
2023-11-08 23:41:04 +0000

    building more of the selector UI

--- components/shared/workoutpreview.tsx
+++ components/shared/workoutpreview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export type WorkoutPreviewProps = {
   id: string;
   name: string;
@@ -10,11 +11,17 @@ export type WorkoutPreviewProps = {
 
 const WorkoutPreview: React.FC<{ wkp: WorkoutPreviewProps }> = ({ wkp }) => {
   return (
-    <div className = "border px-4 py-4">
-      <div className = "text-4xl mb-10">{wkp.name}</div>
-      <div className = "text-lg max-w-md mb-4">{wkp.overview}</div>
-      <div className = "text-lg max-w-md mb-4 flex">Objective: <div className="ml-2"></div>{wkp.objective}</div>
-      <div className = "text-lg max-w-md mb-4 flex">Training Phase: <div className="ml-2"></div>{wkp.training_phase}</div>
+    <div className = "border px-4 py-4" >
+      <div className = "text-4xl mb-4 font-roboto">{wkp.name}</div>
+      <div className = "italic text-sm font-lato">Workout Time: 1:02:00</div>
+      <div className = "italic text-sm font-lato mb-6">Average Intensity: 4.2/10</div>
+
+      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">overview</div>
+      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.overview}</div>
+      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">objective</div>
+      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.objective}</div>
+      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">training phase</div>
+      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.training_phase}</div>
     </div>
   );
 };
--- pages/workout.tsx
+++ pages/workout.tsx
@@ -2,10 +2,14 @@ import { prisma } from '@/lib/prisma';
 
 import "@/app/globals.css";
 
+import { sfPro, inter, orbitron, roboto, montserrat, lato, lora } from "@/app/fonts";
+import cx from "classnames";
+
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import WorkoutPreview, {WorkoutPreviewProps} from '@/components/shared/workoutpreview';
 import MediumTitle from '@/components/shared/mediumtitle';
 import WorkoutSelector from '@/components/shared/workoutselector';
+import { useState, useEffect } from "react";
 
 export const getStaticProps: GetStaticProps = async () => {
   const workouts = await prisma.workout.findMany();
@@ -22,7 +26,7 @@ type Props = {
 const Workout: React.FC<Props> = (props) => {
   return (
     <div className="fixed h-screen w-full bg-cyclists bg-cover bg-center bg-no-repeat">
-      <div className="page">
+      <div className={cx(sfPro.variable, inter.variable, orbitron.variable, roboto.variable, montserrat.variable, lato.variable, lora.variable)}>
         <main className="flex min-h-screen w-full flex-col content-center items-center">
           <div className="bg-white rounded px-10 py-10 mt-20">
             <div className ="w-full text-center text-black">
