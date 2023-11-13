ad605d981c8417390d9bb7d82ef4e8a707ca1acb
Glarff <michael.ross444@gmail.com>
2023-11-13 14:13:59 +0000

    added placeholder start button to workout page

--- components/shared/workoutpreview.tsx
+++ components/shared/workoutpreview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Button from "./button";
 
 export type WorkoutPreviewProps = {
   id: string;
@@ -11,17 +11,31 @@ export type WorkoutPreviewProps = {
 
 const WorkoutPreview: React.FC<{ wkp: WorkoutPreviewProps }> = ({ wkp }) => {
   return (
-    <div className = "border px-4 py-4" >
-      <div className = "text-4xl mb-4 font-roboto">{wkp.name}</div>
-      <div className = "italic text-sm font-lato">Workout Time: 1:02:00</div>
-      <div className = "italic text-sm font-lato mb-6">Average Intensity: 4.2/10</div>
-
-      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">overview</div>
-      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.overview}</div>
-      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">objective</div>
-      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.objective}</div>
-      <div className = "text-sm uppercase text-red-500 font-bold font-roboto">training phase</div>
-      <div className = "text-sm max-w-md mb-4 font-lora">{wkp.training_phase}</div>
+    <div className="border px-4 py-4">
+      <div className="mb-4 font-roboto text-4xl">{wkp.name}</div>
+      <div className="font-lato text-sm italic">Workout Time: 1:02:00</div>
+      <div className="mb-6 font-lato text-sm italic">
+        Average Intensity: 4.2/10
+      </div>
+      <div className="font-roboto text-sm font-bold uppercase text-red-500">
+        overview
+      </div>
+      <div className="mb-4 max-w-md font-lora text-sm">{wkp.overview}</div>
+      <div className="font-roboto text-sm font-bold uppercase text-red-500">
+        objective
+      </div>
+      <div className="mb-4 max-w-md font-lora text-sm">{wkp.objective}</div>
+      <div className="font-roboto text-sm font-bold uppercase text-red-500">
+        training phase
+      </div>
+      <div className="mb-4 max-w-md font-lora text-sm">
+        {wkp.training_phase}
+      </div>
+      <div className="relative mb-16 mt-8 max-w-full">
+        <div className="absolute right-0">
+          <Button>Start</Button>
+        </div>
+      </div>
     </div>
   );
 };
