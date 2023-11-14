da29b7a4814d3fa52856fcaf8eb4ab8a12eb2411
Glarff <michael.ross444@gmail.com>
2023-11-14 03:32:37 +0000

    added prisma model for focus and segments

---
+++ app/data/stockworkouts.ts
@@ -0,0 +1,208 @@
+// ========================= Segment Generation ============================ //
+
+const newSegment = (ttl: string, dur: number, int: number, tps: string) => {
+  return { name: ttl, duration: dur, intensity: int, tip: tps };
+};
+
+// ======================== Random Tip Generation ========================== //
+
+const getRandomElement = (arr: any[]) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
+const getTipByType = (typ: string) => {
+  let possibilities = [];
+
+  if (typ == "Biking") {
+    possibilities.push(
+      "Think about pedaling as if tracing a square.  Push forward along the top, down the frontside, scrape the bottom, then pull back up the backside.",
+    );
+    possibilities.push(
+      "Avoid side to side movement in the knees by keeping core tight - no Gumby riding.",
+    );
+    possibilities.push(
+      "Dont let your pelvis slouch. Rotate the hips forward and push the butt back, similar to plank.",
+    );
+    possibilities.push(
+      "Keep the grip on the bars relaxed.  No white knuckles!  Change grips often if needed.",
+    );
+    possibilities.push(
+      "Pay attention!  Are all parts of your form good?  Let go of unneeded thoughts and focus on finishing the effort.",
+    );
+    possibilities.push(
+      "Get on the pedals early - start pushing forward before 12 oclock position.  Feet flat or toes slightly pointed up.",
+    );
+    possibilities.push(
+      "Keep your head up, relax your elbows, shoulders, and hands slightly.",
+    );
+
+    return getRandomElement(possibilities);
+  } else if (typ == "Productivity") {
+    possibilities.push(
+      "Focus on one goal at a time.  You have all this time to do this one task!",
+    );
+    possibilities.push(
+      "If you get distracted by something, note down to do it later, rather than doing it now.",
+    );
+    possibilities.push(
+      "Take short breaks if needed to clear your mind and rest, but dont start something else.",
+    );
+    possibilities.push(
+      "Before starting something new, quickly think about what the most effecient way to execute it is, then go. Dont overthink it.",
+    );
+    possibilities.push(
+      "Finish your task completely.  If theres time left, take a quick break and relax before the next segment.",
+    );
+
+    return getRandomElement(possibilities);
+  } else if (typ == "FoamRoll") {
+    possibilities.push(
+      "Move up and down the roll for five to ten reps, holding at the end of each move for a few seconds, then switch sides and repeat.",
+    );
+    possibilities.push(
+      "When you hit a tight spot that is painful or uncomfortable, HOLD on that spot for 30-45 seconds. You should feel the tension release slowly",
+    );
+    possibilities.push(
+      "Make sure to keep breathing, even when its painful. Holding your breath wont allow the muscles to release and relax.",
+    );
+    possibilities.push(
+      "RELAX the muscle as best you can. If you are flexing or tensing the muscle group you are trying to roll out, you wont feel the trigger points you need to release",
+    );
+
+    return getRandomElement(possibilities);
+  }
+};
+   
+   
+// =========================== Wu-T1 warm up =========================== //
+
+var warmUp1s1 = newSegment("Warm Up: Z1", 300000, 2, "Spend the first 5 " +
+    "minutes of your session in Z1. Gradually increase cadence to 90+.");
+
+var warmUp1s2 = newSegment("Warm Up: Z2-Z3", 300000, 4, "Spend the next " +
+    "5 minutes progressing through Z2. End at a low Z3.");
+
+var warmUp1s3 = newSegment("Warm Up: Max Spin / Easy Spin", 120000, 5,
+    "Now increase the cadence to your maximum, hold for 5 seconds, " +
+    "followed by 25 seconds easy spin. Repeat maximum cadence / easy " +
+    "spin 3 more times.");
+
+var warmUp1s4 = newSegment("Warm Up: Easy Spin", 120000, 2, "Finish with " +
+    " 2 minute easy spin before starting main content of session.");
+
+var warmUp1 = [warmUp1s1, warmUp1s2, warmUp1s3, warmUp1s4];
+
+// ===================================================================== //
+
+// ========================== 20 min warm up =========================== //
+
+const warmUp2s1 = newSegment("90 RPM", 300000, 2, "Smooth pedaling - clear your " +
+    "mind and mentally prepare for the upcoming session.",);
+
+const warmUp2s2 = newSegment("Easy Spin", 300000, 1, getTipByType("Biking"));
+
+const warmUp2s3 = newSegment("95 RPM", 120000, 2, getTipByType("Biking"));
+
+const warmUp2s4 = newSegment("100 RPM", 120000, 3, getTipByType("Biking"));
+
+const warmUp2s5 = newSegment("105 RPM", 120000, 3, getTipByType("Biking"));
+
+const warmUp2s6 = newSegment("110 RPM", 90000, 4, getTipByType("Biking"));
+
+const warmUp2s7 = newSegment("120-130 RPM", 30000, 4, getTipByType("Biking"));
+
+const warmUp2s9 = newSegment("Max Spin!", 6000, 5, getTipByType("Biking"));
+
+const warmUp2 = [
+  warmUp2s1,
+  warmUp2s3,
+  warmUp2s4,
+  warmUp2s5,
+  warmUp2s6,
+  warmUp2s7,
+  warmUp2s1,
+  warmUp2s9,
+  warmUp2s2,
+  warmUp2s9,
+  warmUp2s2,
+  warmUp2s9,
+  warmUp2s2,
+];
+
+// ====================== Tempo Intervals Workout ====================== //
+
+// Segments 
+const w2s1 = newSegment("HRZ 3", 600000, 5, getTipByType("Biking"));
+const w2s2 = newSegment("Easy Spin", 300000, 2, getTipByType("Biking"));
+const w2s3 = newSegment("Easy Spin", 600000, 2, getTipByType("Biking"));
+
+// Workout
+export const w1segs = (warmUp1.concat([w2s1, w2s2, w2s1, w2s2, w2s1, w2s3]));
+
+// ===================================================================== //
+
+// ======================= Sweet-Spot Intervals ======================== //
+
+// Segments 
+const w3s1 = newSegment("Sweet-Spot", 300000, 6, getTipByType("Biking"));
+
+const w3s2 = newSegment("Recovery", 60000, 1, "Maintain a smooth pedal " +
+    "stroke. Reduce resistance and keep your legs turning over.");
+
+const w3s3 = newSegment("Sweet-Spot", 180000, 6, "Maintain a smooth pedal " +
+    "stroke. Don't stromp on the pedals when you get tired. Pace your " +
+    "effort evenly and avoid major fluctuations in heart rate.");
+
+const w3s4 = newSegment("Cool Down", 600000, 1, "Maintain a smooth pedal " +
+    "stroke. Reduce resistance and keep your legs turning over.");
+
+// Workout
+export const w2segs = warmUp2.concat([w3s1, w3s2, w3s1, w3s2, w3s3, w3s2,
+        w3s3, w3s2, w3s3, w3s2, w3s3, w3s2, w3s3, w3s2, w3s4]);
+
+// ===================================================================== //
+
+// ======================== Pyramid Intervals ========================== //
+
+// Segments
+const w4s1 = newSegment("Threshold", 1 * 60 * 1000, 7, getTipByType("Biking"));
+const w4s2 = newSegment("Recovery", 1 * 60 * 1000, 1, getTipByType("Biking"));
+const w4s3 = newSegment("Threshold", 2 * 60 * 1000, 7, getTipByType("Biking"));
+const w4s4 = newSegment("Recovery", 2 * 60 * 1000, 1, getTipByType("Biking"));
+const w4s5 = newSegment("Threshold", 3 * 60 * 1000, 7, getTipByType("Biking"));
+const w4s6 = newSegment("Recovery", 3 * 60 * 1000, 1, getTipByType("Biking"));
+const w4s7 = newSegment("Threshold", 4 * 60 * 1000, 7, getTipByType("Biking"));
+const w4s8 = newSegment("Recovery", 4 * 60 * 1000, 1, getTipByType("Biking"));
+const w4s9 = newSegment("Threshold", 5 * 60 * 1000, 7, getTipByType("Biking"));
+const w4s10 = newSegment("Cool Down", 10 * 60 * 1000, 1, getTipByType("Productivity"));
+
+// Workout
+export const w3segs = warmUp2.concat([w4s1, w4s2, w4s3, w4s4, w4s5,
+  w4s6, w4s7, w4s8, w4s9, w4s8, w4s7, w4s6, w4s5, w4s4, w4s3, w4s2,
+  w4s1, w4s10]);
+
+// ====================================================================== //
+
+// =========================== 3x10 Minutes ============================= //
+
+// Segments
+const w5s1 = newSegment("HRZ/PZ 3/4", 10 * 60 * 1000, 6, getTipByType("Biking"));
+const w5s2 = newSegment("Recovery", 10 * 60 * 1000, 2, getTipByType("Biking"));
+const w5s3 = newSegment("Cool Down", 10 * 60 * 1000, 1, getTipByType("Productivity"));
+
+// Workout
+export const w4segs = warmUp2.concat([w5s1, w5s2, w5s1, w5s2, w5s1, w5s3]);
+
+// ====================================================================== //
+
+// =========================== 2x20 Minutes ============================= //
+
+// Segments
+const w6s1 = newSegment("Sweet Sport / Threshold", 20 * 60 * 1000, 6, getTipByType("Biking"));
+const w6s2 = newSegment("Recovery", 10 * 60 * 1000, 2, getTipByType("Biking"));
+const w6s3 = newSegment("Cool Down", 10 * 60 * 1000, 1, getTipByType("Productivity"));
+
+// Workout
+export const w5segs = warmUp2.concat([w6s1, w5s2, w6s1, w5s3]);
+
+// ====================================================================== //
+
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,199 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { w1segs, w2segs, w3segs, w4segs, w5segs } from "@/app/data/stockworkouts";
 
-import { stockWorkouts } from 'data/stockworkouts'
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.workout.create({
-      data: {
-        id: "999",
-        name: "test1",
-        overview: "test2",
-        objective: "test3",
-        training_phase: "test4"
+  const w1 = await prisma.workout.upsert({
+    where: { workout_key: "WK01" },
+    update: {},
+    create: {
+      workout_key: "WK01",
+      name: "Tempo Intervals",
+      overview:
+        "Maintain a cadence of 90 rpm+ during the tempo efforts and look to ride low - mid HRZ 3 for the majority of each one. Just spin easily for the recoveries.",
+      objective:
+        "Developing your ability to ride at tempo and an introduction to interval training.",
+      training_phase:
+        "In the early stages of our Improvers Training Plan or as a light workout.",
+      focus: {
+        create: [
+          {
+            focus_text:
+              "Keep your legs turning over during the recoveries and reduce your gear/resistance.",
+          },
+          {
+            focus_text:
+              "Maintain a smooth pedal stroke, especially during the HRZ 3 effort.",
+          },
+          {
+            focus_text: "Don’t stomp on the pedals when you get tired.",
+          },
+          {
+            focus_text:
+              "Pace the tempo efforts evenly and avoid major fluctuations in heart rate.",
+          },
+        ],
+      },
+      segments: {
+        create: w1segs,
+      },
     },
-    })
-  
-    await prisma.workout.createMany({
-      data: stockWorkouts,
-    })
-    await prisma.user.createMany({
-      data: {
-        id: "1",
-        name: "Glarffo"
-      }
-    })
-  }
-  
-  main()
-    .catch(e => {
-      console.error(e)
-      process.exit(1)
-    })
-    .finally(async () => {
-      await prisma.$disconnect()
-    })
+  });
+
+  const w2 = await prisma.workout.upsert({
+    where: { workout_key: "WK02" },
+    update: {},
+    create: {
+      workout_key: "WK02",
+      name: "Zone Build",
+      overview:
+        "Aim to hit the heart rate zones described below. Build through the zone gradually and avoid sudden changes in effort, there will always be a time lag before your heart rate responds. Maintain a 90 rpm+ cadence throughout.",
+      objective: "Developing a feel for your training zones.",
+      training_phase:
+        "In the early stages of our Improvers Training Plan or as a light workout.",
+      focus: {
+        create: [
+          {
+            focus_text:
+              "Concentrate on a strong but even pedal stroke, don’t let your technique slip if you get tired.",
+          },
+          {
+            focus_text:
+              "Aim to hit the middle of the Zone midway through each effort and, if you feel good, push on a bit towards the top end.",
+          },
+          {
+            focus_text:
+              "Keep your cadence up during the Zone 1 blocks, back your gear and resistance right off to allow this.",
+          },
+        ],
+      },
+      segments: {
+        create: w2segs,
+      },
+    },
+  });
+
+  const w3 = await prisma.workout.upsert({
+    where: { workout_key: "WK03" },
+    update: {},
+    create: {
+      workout_key: "WK03",
+      name: "Pyramid Intervals 1",
+      overview:
+        "Efforts should be ridden in HRZ/PZ 4 and at 95 rpm+. Drop resistance and gearing and look to spin in HRZ/PZ 1 for the active recoveries.",
+      objective:
+        "A threshold boosting workout that is excellent for developing your ability to quickly find and maintain this key intensity.",
+      training_phase: "Anytime, both in and out of season.",
+      focus: {
+        create: [
+          {
+            focus_text:
+              "Having accurate and up to date training zones are key for this session so make sure you test for FTHR/FTP regularly.",
+          },
+          {
+            focus_text:
+              "Pace the efforts so there’s no drop off. Power, heart rate, speed or intensity should be consistent through the session.",
+          },
+          {
+            focus_text:
+              "If you’re training for a time trial, perform the efforts in race position.",
+          },
+        ],
+      },
+      segments: {
+        create: w3segs,
+      },
+    },
+  });
+
+  const w4 = await prisma.workout.upsert({
+    where: { workout_key: "WK04" },
+    update: {},
+    create: {
+      workout_key: "WK04",
+      name: "3 x 10 Minutes",
+      overview:
+        "Cadence 90 rpm + during the efforts and “Sweet-Spot” is HRZ/PZ high 3 - low 4. Allow both to drop to easy spinning during recovery.",
+      objective:
+        "A great stepping stone to the classic 2 x 20 minutes session.",
+      training_phase:
+        "Whenever you are looking to build up your ability to ride at “Sweet-Spot” and Threshold.",
+      focus: {
+        create: [
+          {
+            focus_text:
+              "Pace the efforts as evenly as possible, don’t go off too hard and maintain a consistent cadence.",
+          },
+          {
+            focus_text:
+              "Try to hold a stable racing position without excessive movement of the upper body.",
+          },
+          {
+            focus_text:
+              "Make sure you have a bottle of water to hand as these are fairly long efforts.",
+          },
+          {
+            focus_text:
+              "As you get stronger and more confident with the session, try to ride predominately in HRZ/PZ 4.",
+          },
+        ],
+      },
+      segments: {
+        create: w4segs,
+      },
+    },
+  });
+
+  const w5 = await prisma.workout.upsert({
+    where: { workout_key: "WK05" },
+    update: {},
+    create: {
+      workout_key: "WK05",
+      name: "2 x 20 Minutes",
+      overview:
+        "Cadence should be 90 rpm + during the efforts but allow to drop during recovery. If aiming for “Sweet-spot”, work in HRZ/PZ high Zone 3 - mid Zone 4. If aiming for threshold, work as close to 100% FTHR/FTP as possible.",
+      objective:
+        "A classic indoor trainer session that is hard to beat for raising threshold and learning to stay focussed and pace long efforts at this key intensity.",
+      training_phase:
+        "Work through the session at “sweet-spot” intensity during the early off-season and then build up to completing it at FTP/FTHR as intensity goes up and volume comes down.",
+      focus: {
+        create: [
+          {
+            focus_text:
+              "Pace the efforts as evenly as possible, don’t go off too hard and maintain a consistent cadence.",
+          },
+          {
+            focus_text:
+              "Try to hold a stable racing position without excessive movement of the upper body. If training for time trials, use your race position.",
+          },
+          {
+            focus_text:
+              "Make sure you have a bottle of water to hand as these are fairly long efforts.",
+          },
+        ],
+      },
+      segments: {
+        create: w5segs,
+      },
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { id: "1" },
+    update: {},
+    create: {
+      id: "1",
+      name: "Glarffo",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
